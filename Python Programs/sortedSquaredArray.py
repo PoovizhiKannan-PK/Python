5fb9b47a3914114fb079dee266b9d41c52add576
PoovizhiKannan-PK <59499706+PoovizhiKannan-PK@users.noreply.github.com>
2021-08-22 01:21:41 +0530

    SortedSquaredArray

--- Python Programs/sortedSquaredArray.py
+++ Python Programs/sortedSquaredArray.py
@@ -1,5 +1,5 @@
 
-def sortedSquaredArray(n, arr):
+def memoryEfficientSortedSquaredArray(n, arr):
     if arr == [] or n == 1:
         return arr;
     else:
@@ -8,10 +8,26 @@ def sortedSquaredArray(n, arr):
         arr.sort()
         return arr
 
+def timeEfficientSortedSquaredArray(n, arr):
+    if arr == [] or n == 1:
+        return arr
+    else:
+        outarr = [n] * 0
+        i = 0
+        j = n-1
+        for k in range(n):
+            if(abs(arr[i]) > abs(arr[j])):
+                outarr[n-k-1] = arr[i] * arr[i]
+                i += 1
+            else:
+                outarr[n-k-1] = arr[j] * arr[j]
+                j += 1
+        return outarr
 
 arr = []
 n = int(input("Enter no of items"))
 for i in range(n):
     arr.append(int(input(str(i)+" th Element: ")))
 
-print(sortedSquaredArray(n, arr))
+print(memoryEfficientSortedSquaredArray(n, arr))
+print(timeEfficientSortedSquaredArray(n, arr))
