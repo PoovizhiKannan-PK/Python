4e3961c012d7ab89f917493867f0424dc781c52f
PoovizhiKannan-PK <59499706+PoovizhiKannan-PK@users.noreply.github.com>
2021-09-02 00:03:03 +0530

    Update anagram_Fibonacci.py

--- Python Programs/anagram_Fibonacci.py
+++ Python Programs/anagram_Fibonacci.py
@@ -1,9 +1,13 @@
 # Anagrams
-
 str1 = "asdf"
 str2 = "fdsade"
 
 print(set(str1) == set(str2))
+
+# Remove Duplicates
+str3 = "asssdddfdrergvnfhnhggf"
+
+print(list(set(str3)))
 
 # Fibonacci
 n = 5
