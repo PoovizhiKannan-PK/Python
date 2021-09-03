eddfdfb04d88a7cf216c848837c2b5a3c2385ab2
PoovizhiKannan-PK <59499706+PoovizhiKannan-PK@users.noreply.github.com>
2021-09-03 23:35:52 +0530

    Create maximumSumSubArray.py

---
+++ Python Programs/maximumSumSubArray.py
@@ -0,0 +1,12 @@
+# For a given list of numbers[-1, 2, 3, -4], find the contiguous sub array with maximum sum
+# Here max sum is 5 [2,3], if we add the whole array, its 0
+
+lst = [-1, 5, 2, -4, 6]
+
+maxSum = lst[0]
+currentSum = maxSum
+for i in range(1, len(lst)):
+    currentSum = max(lst[i], (lst[i]+currentSum))
+    maxSum = max(currentSum, maxSum)
+
+print(maxSum)
