9939bde6fbe9f6c4a8718a4628ee52f28a34f406
PoovizhiKannan-PK <59499706+PoovizhiKannan-PK@users.noreply.github.com>
2021-09-04 06:23:04 +0530

    Update jewelsAndStones.py

--- Python Programs/jewelsAndStones.py
+++ Python Programs/jewelsAndStones.py
@@ -2,20 +2,25 @@
 # Here the answer is 5, since a(string 2) occurs 3 times and A(string 2) occurs 2 times in String1. So 3 + 2 = 5
 # This is a leetcode problem, check for "Jewels and Stones" to better understand the problem
 
-j = "B"
+j = "aAbc"
 s = "aAAbbb"
-stones = {}
-for i in s:
-    if i in stones.keys():
-        stones[i] += 1
-    else:
-        stones[i] = 1
-
 jewels = 0
+# stones = {}
+# for i in s:
+#     if i in stones.keys():
+#         stones[i] += 1
+#     else:
+#         stones[i] = 1
 
-for i in j:
-    if i in stones.keys():
-        jewels += stones[i]
+# jewels = 0
+
+# for i in j:
+#     if i in stones.keys():
+#         jewels += stones[i]
+
+for i in range(len(s)):
+    if s[i] in j:
+        jewels += 1
 
 print(jewels)
 
